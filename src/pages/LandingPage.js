55499e3e90e041a8099a2f438e6b52b0867265ed
ramiouse <43832932+ramiouse@users.noreply.github.com>
2022-06-06 14:33:42 +0700

    slicing-figma-to-react-homepage
    steps 2

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
+import landingPage from "json/landingPage.json";
+import Hero from "parts/Hero";
 
 // export with extends to component
 export default class LandingPage extends Component {
   render() {
+    // console.log(this.props);
     return (
       <>
         {/* ... : syntax es6, Kalo ada props dari kelas ini akan di masukan juga ke dalam Header class*/}
         <Header {...this.props}></Header>
+        {/* get object data hero dari landingPage json */}
+        <Hero data={landingPage.hero} />
       </>
     );
   }
