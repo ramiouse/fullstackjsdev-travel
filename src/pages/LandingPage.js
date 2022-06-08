1e5e77ddfc7ff92de5f59adbadb48cb691908dee
ramiouse <43832932+ramiouse@users.noreply.github.com>
2022-06-08 14:25:50 +0700

    steps-mostpicked-01
    mostpicked-page-1

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
 // export with extends to component
 export default class LandingPage extends Component {
@@ -12,6 +13,7 @@ export default class LandingPage extends Component {
         <Header {...this.props}></Header>
         {/* get object data hero dari landingPage json */}
         <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
