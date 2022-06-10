7b531158b9eedc9d3d89584e8e0f832d6f763792
ramiouse <43832932+ramiouse@users.noreply.github.com>
2022-06-10 14:21:49 +0700

    star-steps-1
    component star steps 1

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
 
 // export with extends to component
 export default class LandingPage extends Component {
@@ -23,6 +24,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony></Testimony>
       </>
     );
   }
