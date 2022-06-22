46e7ed74a39e0bab6dbea9f77ed79d063e59acf9
ramiouse <43832932+ramiouse@users.noreply.github.com>
2022-06-22 14:16:29 +0700

    elements inputdate - 02
    create element function and add styles css 01

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,94 @@
+import React, { useState, useRef, useEffect } from "react";
+import propTypes from "prop-types";
+
+import { DateRange } from "react-date-range";
+
+import "./index.scss";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import formatDate from "utils/formatDate";
+import iconCalendar from "assets/images/icons/ic_calendar.svg";
+
+export default function Date(props) {
+  const { value, placeholder, name } = props;
+  const [isShowed, setIsShowed] = useState(false);
+
+  const datePickerChange = (value) => {
+    const target = {
+      target: {
+        value: value.selection,
+        name: name,
+      },
+    };
+
+    props.onChange(target);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  // create new useRef
+  const refDate = useRef(null);
+  const handleClickOutside = (event) => {
+    if (refDate && !refDate.current.contains(event.target)) {
+      setIsShowed(false);
+    }
+  };
+
+  // kalo ada, maka setting function setIsShowed jadi => false
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setIsShowed(false);
+  };
+
+  //   kalo ada enddate, maka tambahin ' - ' untuk pembatas
+  const displayDate = `${value.startDate ? formatDate(value.startDate) : ""}${
+    value.endDate ? " - " + formatDate(value.endDate) : ""
+  }`;
+
+  // return html as show element
+  return;
+  <div
+    ref={refDate}
+    className={["input-date mb-3", props.outerClassName].join(" ")}
+  >
+    <div className="input-group">
+      <div className="input-group-prepend bg-gray-900">
+        <span className="input-group-text">
+          <img src={iconCalendar} alt="icon calendar" />
+        </span>
+      </div>
+      <input
+        type="text"
+        readOnly
+        className="form-control"
+        value={displayDate}
+        placeholder={placeholder}
+        onClick={() => setIsShowed(!isShowed)}
+      />
+
+      {isShowed && (
+        <div className="date-range-wrapper">
+          <DateRange
+            editableDateInputs={true}
+            onChange={datePickerChange}
+            moveRangeOnFirstSelection={false}
+            onRangeFocusChange={check}
+            ranges={[value]}
+          />
+        </div>
+      )}
+    </div>
+  </div>;
+}
+
+Date.propTypes = {
+  value: propTypes.object,
+  onChange: propTypes.func,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+};
